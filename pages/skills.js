3401d916e1d06ca58029885ed9ec671a33e8c42a
AVirolainen <58146499+AVirolainen@users.noreply.github.com>
2021-12-13 15:03:08 +0200

    finishing skills page

--- pages/skills.js
+++ pages/skills.js
@@ -14,6 +14,7 @@ import {
 } from '@chakra-ui/react'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
+import { Progress } from 'antd'
 
 const Skills = () => {
   return (
@@ -25,7 +26,7 @@ const Skills = () => {
         </Heading>
         <Paragraph>
           <Flex mt={10}>
-            <Box align="center" fontSize="3xl" w="33%">
+            <Box align="center" fontSize="3xl" w="33%" fontWeight="700">
               <Image
                 src="/languages/python.png"
                 alt="python"
@@ -34,7 +35,7 @@ const Skills = () => {
               />
               Python
             </Box>
-            <Box align="center" fontSize="3xl" w="33%">
+            <Box align="center" fontSize="3xl" w="33%" fontWeight="700">
               <Image
                 src="/languages/js.png"
                 alt="javascript"
@@ -43,7 +44,7 @@ const Skills = () => {
               />
               JavaScript
             </Box>
-            <Box align="center" fontSize="3xl" w="33%">
+            <Box align="center" fontSize="3xl" w="33%" fontWeight="700">
               <Image
                 src="/languages/scala.png"
                 alt="scala"
@@ -51,6 +52,119 @@ const Skills = () => {
                 mb={5}
               />
               Scala
+            </Box>
+          </Flex>
+        </Paragraph>
+        <Heading as="h3" variant="section-title">
+          Databases
+        </Heading>
+        <Paragraph>
+          <Flex mt={10}>
+            <Box align="center" fontSize="3xl" w="50%" fontWeight="700">
+              <Image
+                src="/languages/mdb.png"
+                alt="scala"
+                maxWidth="180px"
+                mb={5}
+              />
+              MongoDB
+            </Box>
+            <Box align="center" fontSize="3xl" w="50%" fontWeight="700">
+              <Image
+                src="/languages/mysql.jpg"
+                alt="scala"
+                maxWidth="180px"
+                mb={5}
+              />
+              MySQL
+            </Box>
+          </Flex>
+        </Paragraph>
+        <Heading as="h3" variant="section-title">
+          Frameworks
+        </Heading>
+        <Paragraph>
+          <Flex mt={10}>
+            <Box align="center" fontSize="3xl" w="33%" fontWeight="700">
+              <Image
+                src="/languages/react.svg"
+                alt="scala"
+                maxWidth="180px"
+                mb={5}
+              />
+              React
+            </Box>
+            <Box align="center" fontSize="3xl" w="33%" fontWeight="700">
+              <Image
+                src="/languages/react-native.jpg"
+                alt="scala"
+                maxWidth="138px"
+                mb={5}
+              />
+              React Native
+            </Box>
+            <Box align="center" fontSize="3xl" w="33%" fontWeight="700">
+              <Image
+                src="/languages/kivy.png"
+                alt="scala"
+                maxWidth="157px"
+                mb={5}
+              />
+              Kivy
+            </Box>
+          </Flex>
+        </Paragraph>
+        <Heading as="h3" variant="section-title">
+          Instruments
+        </Heading>
+        <Paragraph>
+          <Flex mt={10}>
+            <Box align="center" fontSize="3xl" w="33%" fontWeight="700">
+              <Image
+                src="/languages/redux.png"
+                alt="scala"
+                maxWidth="180px"
+                mb={5}
+              />
+              Redux
+            </Box>
+            <Box align="center" fontSize="3xl" w="33%" fontWeight="700">
+              <Image
+                src="/languages/github.png"
+                alt="scala"
+                maxWidth="180px"
+                mb={5}
+              />
+              Github
+            </Box>
+            <Box align="center" fontSize="3xl" w="33%" fontWeight="700">
+              <Image
+                src="/languages/nodejs.png"
+                alt="scala"
+                maxWidth="180px"
+                mb={5}
+              />
+              Node.js
+            </Box>
+          </Flex>
+          <Flex mt={10}>
+            <Box align="center" fontSize="3xl" w="50%" fontWeight="700">
+              <Image
+                src="/languages/sass.png"
+                alt="scala"
+                maxWidth="180px"
+                mb={5}
+              />
+              Sass
+            </Box>
+            <Box align="center" fontSize="3xl" w="50%" fontWeight="700">
+              <Image
+                src="/languages/bootstrap.png"
+                alt="scala"
+                maxWidth="180px"
+                mb={5}
+              />
+              Bootstrap
             </Box>
           </Flex>
         </Paragraph>
