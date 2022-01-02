1fb943ff2a4edda478fb64471894e407be6d20f2
Rudenko Stanislav <rudenkostas2@gmail.com>
2022-01-02 23:44:20 +0200

    adding blovk hooby and prettier

--- pages/hobby.js
+++ pages/hobby.js
@@ -1,4 +1,4 @@
-import { Container, Image, Box } from '@chakra-ui/react'
+import { Container, Image, Box, useColorModeValue } from "@chakra-ui/react";
 
 const Hobby = () => {
   return (
@@ -6,15 +6,29 @@ const Hobby = () => {
       <Box
         align="center"
         mx="auto"
-        mt={2}
+        mt={20}
         fontSize="3xl"
         w="50%"
         fontWeight="700"
       >
         <Image src="/hobby/liverpool.png" alt="scala" maxWidth="250px" mb={5} />
       </Box>
+      <Box
+        borderRadius="lg"
+        border="1px"
+        maxW="container.sm"
+        bg={useColorModeValue("whiteAlpha.800", "whiteAlpha.200")}
+        p={8}
+        fontSize='xl'
+        mx="auto"
+        mb={6}
+        align="center"
+      >
+        From 2016 I am a big fan of one of the greatest football teams in the
+        world - Liverpool
+      </Box>
     </Container>
-  )
-}
+  );
+};
 
-export default Hobby
+export default Hobby;
