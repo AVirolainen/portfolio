8ce81dd63354a7d5b0535eca47babfa0ef69dd4c
Stanislav Rudenko <rudenkostas2@gmail.com>
2022-06-22 00:11:59 +0300

    refactoring script for parsing table

---
+++ scripts/constants.js
@@ -0,0 +1,11 @@
+const Constants = {
+    URL: "https://football.ua/england.html",
+    CLASSES_FOR_PARSE: {
+        TABLE: ".tournament-table",
+        TEAM: ".team",
+        GAMES: ".games",
+        SCORE: ".score"
+    }
+}
+
+module.exports = Constants
--- scripts/getTable.js
+++ scripts/getTable.js
@@ -1,31 +1,38 @@
-const axios = require('axios')  
-const cheerio = require('cheerio')
+const axios = require("axios");
+const cheerio = require("cheerio");
+const Constants = require("./constants")
 
+function parseTable(response){
+    const html = response.data;
+    const htmlToParse = cheerio.load(html);
+    let id = 0;
+    const res = {};
 
-const url = "https://football.ua/england.html"
-module.exports = axios(url)
-    .then(responce => {
-        const html = responce.data
-        const $ = cheerio.load(html)
-        let id = 0
-        const info2 = {}
+    htmlToParse(Constants.CLASSES_FOR_PARSE.TABLE)
+        .children()
+        .each(function () {
+            let team = htmlToParse(Constants.CLASSES_FOR_PARSE.TEAM, this)
+                .text()
+                .replace(/(\r\n|\n|\r|\t)/gm, "")
+                .split(" ")
+                .filter((item) => item.length > 0)
+                .join(" ");
 
-        $(".tournament-table").children().children().each(function(){
-            let team = $(".team", this).text().replace(/(\r\n|\n|\r|\t)/gm, "")
-                                            .split(" ")
-                                            .filter(item => item.length > 0)
-                                            .join(" ")
+            let games = htmlToParse(Constants.CLASSES_FOR_PARSE.GAMES, this).text();
+            let points = htmlToParse(Constants.CLASSES_FOR_PARSE.SCORE, this).text();
+            let image = htmlToParse(Constants.CLASSES_FOR_PARSE.TEAM, this).children().find("img").attr("src");
 
-            let games = $(".games", this).text()
-            let points = $(".score", this).text()
-            let image = $(".team", this).children().find('img').attr('src')
-            
-            if (team.length > 0 && parseInt(games, 10) && parseInt(points, 10)){
-                info2["id_"+id] = {
-                    team, games, points, image
-                }
-                id +=1
-            } 
-        })   
-        return info2
-}).catch(err => console.log(err))
+            if (team.length > 0 && parseInt(games, 10) && parseInt(points, 10)) {
+                res["id_" + id] = {
+                    team,
+                    games,
+                    points,
+                    image,
+                };
+                id += 1;
+            }
+        });
+    return res;
+}
+
+module.exports = axios(Constants.URL).then(response => parseTable(response))
